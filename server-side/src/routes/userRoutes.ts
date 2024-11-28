d815ebf3622829af7c2e3e036f109450d92d5d4a
MairemSo <souayah.mariem@gmail.com>
2024-11-28 06:29:38 +0100

    routes_updated

--- server-side/src/routes/userRoutes.ts
+++ server-side/src/routes/userRoutes.ts
@@ -3,11 +3,11 @@ import { Router } from "express";
 const userRoutes = Router();
 
 userRoutes.get("/", (req, res) => {
-    res.json({ message: "User endpoint works!" });
-})
+  res.json({ message: "User endpoint works!" });
+});
 
-userRoutes.post("/register")
-userRoutes.post('/login')
-userRoutes.get('/profile')
+userRoutes.post("/register");
+userRoutes.post("/login");
+userRoutes.get("/profile");
 
 export default userRoutes;
