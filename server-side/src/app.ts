5de150f4cfd0165d1c99304ce3abdf084b10e6ce
MairemSo <souayah.mariem@gmail.com>
2025-02-03 13:49:33 +0100

    added a resonse for non exsiting routes and an md file for backend apis

--- server-side/src/app.ts
+++ server-side/src/app.ts
@@ -9,12 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
-app.use("/api/recipes", recipesRouter);
-app.use("/api/recipes/:recipeId/comments", commentsRouter);
+app.use("/api/user", userRoutes);
+app.use("/api/recipe", recipesRouter);
+app.use("/api/recipe/:recipeId/comment", commentsRouter);
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ error: error.message });
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 export default app;
